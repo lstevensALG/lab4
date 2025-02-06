15edf78b1b9766cccb75b52e19b91e004394e550
lstevensALG <lstevens@algomau.ca>
2025-02-06 16:23:40 -0500

    Adding all files

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function calculateSquare() {
+    let a = document.getElementById("input").value;
+
+    document.getElementById("output0").textContent = a;
+    document.getElementById("output1").textContent = a * a;
+}
